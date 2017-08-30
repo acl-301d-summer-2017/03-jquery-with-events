ea27fefa99fa3e06aec6f00a0070fcccfb7ff43e
Zac-L <Zachlichens@gmail.com>
2017-08-30 14:22:41 -0700

    populated drop damn, fixed a script tag

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -36,17 +36,18 @@ articleView.handleAuthorFilter = function () {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
+    console.log('author changed')
     if ($(this).val()) {
-      // $(#articles).hide();
-      // $(#author-filter).removeClass(#articles).fadeIn("slow");
+      // var $value = $('#author-filter').val();
+      $('#articles').hide();
+      // $value.fadeIn('slow');
 
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
-      // $(#author-filter).removeClass(#articles).hide();
-      // $(#articles).show();
+      $('#articles').show();
 
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
@@ -88,5 +89,6 @@ articleView.setTeasers = function () {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function () {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
