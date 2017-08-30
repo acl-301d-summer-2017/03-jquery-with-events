bbe5cf0190146e52345fff39bc42ee8cda06b3a5
Shane Moyo <smoyo@pdx.edu>
2017-08-30 15:38:36 -0700

    5 TODOs done

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -46,9 +46,9 @@ articleView.handleAuthorFilter = function() {
       $( 'article[data-author ="' + authorName + '"]' ).show();
 
     } else {
-      // TODO: If the select box was changed to an option that is blank, we should
+      // TODO/DONE: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $( 'article[data-author]' ).show();
     }
     $('#category-filter').val('');
   });
@@ -59,7 +59,15 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $( 'article' ).hide();
+      var categoryName = $(this).val()
+      $( 'article[data-category ="' + categoryName + '"]' ).show();
+    } else {
+      $( 'article[data-author]' ).show();
+    }
+  })
 };
 
 articleView.handleMainNav = function() {
@@ -90,4 +98,5 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
 });
